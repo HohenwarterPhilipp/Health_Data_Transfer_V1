94268cb5cb2a0831a43b69aaf60aba344ec8ab62
HohenwarterPhilipp <phohenw@gmail.com>
2019-08-23 09:20:18 +0200

    f

--- app/src/main/java/com/example/health_data_transfer_v1/pkgData/AppDatabase.java
+++ app/src/main/java/com/example/health_data_transfer_v1/pkgData/AppDatabase.java
@@ -10,9 +10,7 @@ import androidx.room.TypeConverters;
 @Database(entities = {BloodPressureMeasurement.class, ScaleMeasurement.class}, version = 1)
 @TypeConverters({Converters.class})
 public abstract class AppDatabase extends RoomDatabase {
-
     private static AppDatabase INSTANCE;
-
     public abstract BloodPressureMeasurementDao bloodPressureMeasurementDao();
     public abstract ScaleMeasurementDao scaleMeasurementDao();
 
